16a372fc31c0750a0da3c91051fde8fb26821287
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2020-07-03 17:37:04 +0530

    added the header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import {Route, Switch} from 'react-router-dom';
+import Header from './components/header/header-component'
 
 const Hats = (props) => {
   console.log(props);
@@ -30,6 +31,7 @@ const Sneakers = () => (
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
           <Route exact path="/Fashionvilla/" component={Homepage}/>
           <Route exact path="/Fashionvilla/shop" component={ShopPage}/>
